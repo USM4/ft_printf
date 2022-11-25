130d96d0aa4cc971d48722e81fe1621b2d8c28f6
Oussama Redoine <oredoine@e1r13p1.1337.ma>
2022-11-25 19:04:03 +0100

    step 1

---
+++ printf.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   printf.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: oredoine <oredoine@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/25 18:06:22 by oredoine          #+#    #+#             */
+/*   Updated: 2022/11/25 18:06:25 by oredoine         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PRINTF_H
+#define PRINTF_H
+
+#include<unistd.h>
+#include<stdio.h>
+#include<stdarg.h>
+
+int ft_printf(const char *, ...);
+
+#endif
